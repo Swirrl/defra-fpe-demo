6c74a71e1c51371822fe37ea3ccdaf21322d1816
Ninamma Rai <32230328+Ninamma@users.noreply.github.com>
2022-06-27 16:37:07 +0100

    basic map with oxford arc geometry, add prettier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import mapboxgl from "mapbox-gl";
+import { Map, Source, Layer, NavigationControl } from "react-map-gl";
+
+// Needed for production build:
+// https://github.com/visgl/react-map-gl/issues/1266#issuecomment-753686953
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass =
+  require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
+
+const measureScaleArea = require("./geojson/measure-scale-area.json");
+const measures = require("./geojson/measures.json");
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Map
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        initialViewState={{
+          longitude: -2.6,
+          latitude: 53,
+          zoom: 5.1,
+        }}
+        style={{ height: "100vh" }}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+      >
+        <Source id="measureScale" type="geojson" data={measureScaleArea}>
+          <Layer
+            id="measureScaleBorders"
+            source="measureScale"
+            type="line"
+            layout={{ "line-cap": "round", "line-join": "round" }}
+            paint={{
+              "line-width": 3,
+              "line-dasharray": [0.1, 2],
+            }}
+          />
+        </Source>
+        <Source id="measures" type="geojson" data={measures}>
+          <Layer
+            id="measures"
+            source="measuresBorders"
+            type="line"
+            layout={{ "line-cap": "round", "line-join": "round" }}
+            paint={{
+              "line-width": 4,
+            }}
+          />
+        </Source>
+        <NavigationControl />
+      </Map>
+    </>
   );
 }
 
