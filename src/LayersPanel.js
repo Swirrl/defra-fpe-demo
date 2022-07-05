b5c9bf473ce241ccfd90cc685ef5a7d391aa44cb
Ninamma Rai <32230328+Ninamma@users.noreply.github.com>
2022-07-05 16:40:30 +0100

    Move layers panel to top left of map

--- src/LayersPanel.js
+++ src/LayersPanel.js
@@ -13,22 +13,22 @@ export default function LayersPanel({ layerVisibilities, setVisibilities }) {
       className="layers-panel"
       style={{
         position: "absolute",
-        top: "5vh",
-        right: "10vw",
-        width: "20vw",
+        top: "0.5rem",
+        left: "52%",
         padding: "0.5rem",
         boxShadow: "initial",
         backgroundColor: "#fff",
       }}
     >
       {Object.keys(layerVisibilities).map((layer) => (
-        <div key={layer} className="input">
+        <div key={layer} className="input d-flex justify-content-between" >
           <label>{titleCase(layer)}</label>
           <input
             type="checkbox"
             id={layer}
             checked={layerVisibilities[layer] === "visible"}
             onChange={toggleLayer}
+            style={{marginLeft: '1rem'}}
           ></input>
         </div>
       ))}
