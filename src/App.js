bdf81293fb1a99cc4d3f2d7088f3ec99e762c6d4
Ninamma Rai <32230328+Ninamma@users.noreply.github.com>
2022-07-05 19:22:38 +0100

    allow hovering and clicking of overlapping features

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 import mapboxgl from "mapbox-gl";
 import { Map, Source, Layer, NavigationControl } from "react-map-gl";
 import LayersPanel from "./LayersPanel";
-import MeasureList from "./MeasuresList";
+import MeasureList from "./components/MeasuresList";
 
 // Needed for production build:
 // https://github.com/visgl/react-map-gl/issues/1266#issuecomment-753686953
@@ -14,11 +14,65 @@ mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worke
 const measureScaleArea = require("./geojson/measure-scale-area.json");
 const measures = require("./geojson/measures.json");
 
+const LinkOrLabel = ({feature}) => {
+      if (feature.properties['feature-url']) {
+        return (<a id={feature.properties.uri} href={"https://environment.data.gov.uk" + feature.properties['feature-url']} >
+          {feature.properties.label}
+        </a>)
+      } else {
+        return (
+          feature.properties.label
+        )
+      }
+    } 
+
 function App() {
+  const [hoverInfo, setHoverInfo] = useState(null);
+  const [clickInfo, setClickInfo] = useState(null);
+  const [hoverListInfo, setHoverListInfo] = useState(null);
   const [layerVisibilities, setLayerVisibilities] = useState({
-    measureScaleArea: "visible",
+    localAuthority: "visible",
     measures: "visible",
   });
+
+  function debounce(cb, delay = 100){
+    let timeout
+    return (...args) => {
+      clearTimeout(timeout)
+      timeout = setTimeout(() =>{
+        cb(...args)
+      }, delay)
+    }
+  }
+
+  const onHover = (features) => {
+    if (features.length > 0) {
+      setHoverInfo({features});
+    } else {
+      setHoverInfo(null)
+    }}
+
+  const optimisedOnHover = debounce(onHover)
+
+  const onClick = useCallback((event) => {
+    if (event.features.length > 0) {
+      setClickInfo({features: event.features})
+    } else {
+      setClickInfo(null)
+    }
+  }, [])
+
+  const hoverList = useCallback((event) => {
+    const selectedFeature = {uri: event.target.getAttribute("id")}
+    setHoverListInfo([{properties: selectedFeature}])
+  })
+  
+  const hoverLeave = useCallback(() => {
+    setHoverListInfo(null)
+  })
+  const selectedFeatures = hoverListInfo || (clickInfo && clickInfo.features) || (hoverInfo && hoverInfo.features) || []
+  let filter = useMemo(() => ['in', ['get', 'uri'], ['literal', selectedFeatures.map(feature => (feature.properties.uri) || '')]])
+
 
   return (
     <div className="row">
@@ -26,53 +80,119 @@ function App() {
         <MeasureList />
       </div>
       <div className="map col" style={{height: '100vh'}} >
-      <Map
-        mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        initialViewState={{
+        <Map
+          mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+          initialViewState={{
             longitude: 0.13,
             latitude: 51.53,
             zoom: 8,
-        }}
-        style={{ height: "100vh" }}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-      >
-        <Source id="measureScale" type="geojson" data={measureScaleArea}>
-          <Layer
-            id="measureScaleBorders"
-            source="measureScale"
-            type="line"
-            layout={{
-              "line-cap": "round",
-              "line-join": "round",
-              visibility: layerVisibilities.measureScaleArea,
-            }}
-            paint={{
-              "line-width": 3,
-              "line-dasharray": [0.1, 2],
-            }}
-          />
-        </Source>
-        <Source id="measures" type="geojson" data={measures}>
-          <Layer
-            id="measures"
-            source="measuresBorders"
-            type="line"
-            layout={{
-              "line-cap": "round",
-              "line-join": "round",
-              visibility: layerVisibilities.measures,
-            }}
-            paint={{
-              "line-width": 4,
-            }}
-          />
-        </Source>
-        <NavigationControl />
-      </Map>
-      <LayersPanel
-        layerVisibilities={layerVisibilities}
-        setVisibilities={setLayerVisibilities}
-      />
+          }}
+          mapStyle="mapbox://styles/mapbox/streets-v11"
+          interactiveLayerIds={["measureScaleBlank", "measuresBlank"]}
+          onMouseMove={event => optimisedOnHover(event.features)}
+          height="200px"
+          onClick={onClick}
+        >
+          <Source id="measureScale" type="geojson" data={measureScaleArea} generateId={true}>
+            <Layer
+              id="measureScaleBorders"
+              source="measureScale"
+              type="line"
+              layout={{
+                "line-cap": "round",
+                "line-join": "round",
+                visibility: layerVisibilities.localAuthority,
+              }}
+              paint={{
+                "line-width": 3,
+                "line-dasharray": [0.1, 2],
+              }}
+            />
+            <Layer
+              id="measureScaleBlank"
+              source="measureScale"
+              type="fill"
+              layout={{ visibility: layerVisibilities.localAuthority }}
+              paint={{
+                "fill-opacity": 0
+              }}
+            />
+            <Layer
+              id="measureScaleFills"
+              source="measureScale"
+              type="fill"
+              layout={{ visibility: layerVisibilities.localAuthority }}
+              paint={{
+                "fill-color": "black",
+                "fill-opacity": 0.3
+              }}
+              filter={filter}
+            />
+          </Source>
+          <Source id="measures" type="geojson" data={measures} generateId={true}>
+            <Layer
+              id="measures"
+              source="measuresBorders"
+              type="line"
+              layout={{
+                "line-cap": "round",
+                "line-join": "round",
+                visibility: layerVisibilities.measures,
+              }}
+              paint={{
+                "line-width": 4,
+                "line-color": "#2E2E2E"
+              }}
+            />
+            <Layer
+              id="measuresBlank"
+              source="measures"
+              type="fill"
+              layout={{ visibility: layerVisibilities.measures }}
+              paint={{
+                "fill-opacity": 0
+              }}
+            />
+            <Layer
+              id="measuresFills"
+              source="measures"
+              type="fill"
+              layout={{ visibility: layerVisibilities.measures }}
+              paint={{
+                "fill-color": "black",
+                "fill-opacity": 0.3
+              }}
+              filter={filter}
+            />
+          </Source>
+          <NavigationControl />
+          {((clickInfo && clickInfo.features.length > 0) || (hoverInfo && hoverInfo.features.length > 0)) && (
+            <div
+              style={{
+                position: 'absolute',
+                bottom: 0,
+                height: "30%",
+                width: "100%",
+                overflow: 'auto',
+                background: "#fff",
+                fontSize: '1rem',
+                paddingBottom: '1rem'
+              }}
+            >
+              <ul style={{padding: '0.5rem'}}>
+                {(clickInfo || hoverInfo).features.map((feature) => (
+                  <li className="hover-list" style={{listStyle: 'none', paddingBottom: '0.5rem'}} id={feature.properties.uri} onMouseMove={hoverList} onMouseLeave={hoverLeave}>
+                      <LinkOrLabel feature={feature} />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </Map>
+        <LayersPanel
+          layerVisibilities={layerVisibilities}
+          setVisibilities={setLayerVisibilities}
+        />
       </div>
     </div>
   );
