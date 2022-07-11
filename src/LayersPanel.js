63da2cf20868168565858f63f8d3395816ac55d9
Ninamma Rai <32230328+Ninamma@users.noreply.github.com>
2022-07-11 18:37:44 +0100

    add legend for layers

--- src/LayersPanel.js
+++ src/LayersPanel.js
@@ -10,26 +10,16 @@ export default function LayersPanel({ layerVisibilities, setVisibilities }) {
 
   return (
     <div
-      className="layers-panel"
-      style={{
-        position: "absolute",
-        top: "0.5rem",
-        left: "52%",
-        padding: "0.5rem",
-        boxShadow: "initial",
-        backgroundColor: "#fff",
-      }}
-    >
+      className="layers-panel">
       {Object.keys(layerVisibilities).map((layer) => (
-        <div key={layer} className="input d-flex justify-content-between" >
+        <div key={layer} className="legend d-flex justify-content-between align-items-center" >
+          <span className={"legend-" + layer + "-color"} />
           <label>{titleCase(layer)}</label>
           <input
             type="checkbox"
             id={layer}
             checked={layerVisibilities[layer] === "visible"}
-            onChange={toggleLayer}
-            style={{marginLeft: '1rem'}}
-          ></input>
+            onChange={toggleLayer}/>
         </div>
       ))}
     </div>
